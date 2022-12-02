f4f6c81448a59ffee5c190f8d2a225710da0678b
mgetz34 <mgetz340@gmail.com>
2022-12-01 20:50:07 -0700

    added optionsArr to inquirer to prompt user to select command for db query

--- index.js
+++ index.js
@@ -18,15 +18,44 @@ const inquirer = require('inquirer');
 // GIVEN a command - line application that accepts user input
 // WHEN I start the application
 // THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-const optionsArr = ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"];
+const optionsArr = [
+    {
+        name: "view all departments",
+        value: null
+    },
+    {
+        name: "view all roles",
+        value: null
+
+    },
+    {
+        name: "view all employees",
+        value: null
+    },
+    {
+        name: "add a department",
+        value: null
+    },
+    {
+        name: "add a role",
+        value: null
+    },
+    {
+        name: "add an employee",
+        value: null
+    },
+    {
+        name: "update an employee role",
+        value: null
+    }];
 
 inquirer
     .prompt([
         {
-            type: 'input',
+            type: 'list',
             name: 'options',
             message: "What would you like to do?",
-            options: optionsArr
+            choices: optionsArr
         }
     ])
 

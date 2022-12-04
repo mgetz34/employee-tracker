943280ce29b4b2534e469e91a9783a129f03cb1c
mgetz34 <mgetz340@gmail.com>
2022-12-03 20:36:02 -0700

    moved acceptance criteria comments to pertinant locations throughout index.js

--- index.js
+++ index.js
@@ -66,23 +66,29 @@ function start() {
             }
         })
 };
-
+// WHEN I choose to view all departments
+// THEN I am presented with a formatted table showing department names and department ids
 function viewDepartments() {
     db.query('SELECT * FROM department', function (err, results) {
         console.table(results)
     });
 };
+// WHEN I choose to view all roles
+// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
 function viewRoles() {
     db.query('SELECT * FROM role', function (err, results) {
         console.table(results);
     });
 };
+// WHEN I choose to view all employees
+// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 function viewEmployees() {
     db.query('SELECT * FROM employee', function (err, results) {
         console.table(results);
     });
 };
-
+// WHEN I choose to add a department
+// THEN I am prompted to enter the name of the department and that department is added to the database
 function addDepartment() {
     inquirer
         .prompt([
@@ -120,7 +126,8 @@ function deleteDepartment() {
             })
         })
 };
-
+// WHEN I choose to add a role
+// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
 function addRole() {
     inquirer
         .prompt([
@@ -168,7 +175,8 @@ function deleteRole() {
             })
         })
 };
-
+// WHEN I choose to add an employee
+// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
 function addEmployee() {
     inquirer
         .prompt([
@@ -222,5 +230,8 @@ function deleteEmployee() {
             })
         })
 };
+// WHEN I choose to update an employee role
+// THEN I am prompted to select an employee to update and their new role and this information is updated in the datab
+function updateEmployee() {
 
-// function updateEmployee();
+}
